39b2ef2ab39cbaabe27d57963b804a7a3dc4bef4
Sam Bao <sambao21@gmail.com>
2013-08-27 06:38:12 -0500

    return key and secret in response

--- oauth_header_tool.rb
+++ oauth_header_tool.rb
@@ -4,7 +4,7 @@ set :haml, :format => :html5
 
 get '/' do
   if params[:oauth_key] and params[:oauth_secret]
-    @oauth_header = 'aasdflkjasfdjlaksjdflj'
+    @oauth_header = "#{params[:oauth_key]}::#{params[:oauth_secret]}"
   end
   haml :index
 end
